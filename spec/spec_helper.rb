c120bbb933b895abc7af56f53e9c8872ea5a8a61
Thiago Ribeiro <thiagitosouza@gmail.com>
2017-06-29 16:33:32 -0300

    add formatter to show coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'br_invoices_pdf'
-require 'simplecov'
 require 'coveralls'
+require 'simplecov'
 Coveralls.wear!
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+])
 SimpleCov.start
