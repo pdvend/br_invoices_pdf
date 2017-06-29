fc1f24ccfea970e0fa43ae6fa741c32dae8bbf93
Thiago Ribeiro <thiagitosouza@gmail.com>
2017-06-29 16:56:58 -0300

    fix requires in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'br_invoices_pdf'
-require 'coveralls'
 require 'simplecov'
+require 'coveralls'
 Coveralls.wear!
+require 'br_invoices_pdf'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
