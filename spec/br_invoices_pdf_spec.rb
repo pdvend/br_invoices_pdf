dd4d53c87e2daf494d108f3293d613f9fea5e013
Gabriel Teles <gab.teles@hotmail.com>
2017-06-30 16:57:13 -0300

    [test] splitting version test

--- spec/br_invoices_pdf_spec.rb
+++ spec/br_invoices_pdf_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
 describe BrInvoicesPdf do
-  it 'has a version number' do
-    expect(BrInvoicesPdf::VERSION).not_to be nil
-  end
-
   before do
     # Clean registered generators
     BrInvoicesPdf.instance_variable_set(:@generators, {})
---
+++ spec/version_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe BrInvoicesPdf::VERSION do
+  it 'has a version number' do
+    is_expected.to_not be_nil
+  end
+end
