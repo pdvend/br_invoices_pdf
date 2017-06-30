df8380ae2a8150639cab0cda625350d16a6ec955
Gabriel Teles <gab.teles@hotmail.com>
2017-06-30 16:54:00 -0300

    [test] adding errors to BrInvoicesPdf::Errors::InvalidDocumentType

---
+++ spec/errors/invalid_document_type_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe BrInvoicesPdf::Errors::InvalidDocumentType do
+  describe '#initialize' do
+    subject { described_class.new(type) }
+
+    before do
+      # Fill generators with some fake generator
+      BrInvoicesPdf.instance_variable_set(:@generators, test: double('generator'))
+    end
+
+    let(:type) { :foo }
+
+    it { expect { subject }.to_not raise_error }
+    it { expect(subject.message).to eq("`:foo` is not supported. Must be one of [:test]") }
+  end
+end
