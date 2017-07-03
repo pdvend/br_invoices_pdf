c6ea1464107c189bc7ce6dc24bcbf579f890460b
Thiago Ribeiro <thiagitosouza@gmail.com>
2017-07-03 11:52:11 -0300

    add all necessary fields

--- lib/br_invoices_pdf/cfe/parser.rb
+++ lib/br_invoices_pdf/cfe/parser.rb
@@ -3,36 +3,56 @@ module BrInvoicesPdf
     module Parser
       module_function
 
-      # atributos
-      # city -> emit,enderEmit
-      # company_state -> ?
-      # cpf -> ide,NFCE
-      # total de items em RS->
-      # valor total
-      # desconto total
-      # total bruto de items
-      # total desconto/acressimo sobre item
-      # obs do fisco
-      # note
-      # valor aproximado de tributos
-      # sat number
-      # hora da emissao
-      # chave de acesso
-      # codigo de barras 1,2 e qrcode
-      def parse(_xml)
+      AVAILABLE_UF = { '35' => 'São Paulo' }.freeze
+      SAT_QRCODE_SEPARATOR   = '|'.freeze
+      def parse(xml)
         # Retornar uma hash com todos os atributos necessarios
         {
+          uf: AVAILABLE_UF[xml.locate('infCFe/ide/cUF').first_text],
+          access_key: xml.locate('Signature/SignedInfo').first.nodes.last.attributes[:URI]
+          sat_number: xml.locate('infCFe/ide/nserieSAT').first.text,
+          emission_date: xml.locate('infCFe/ide/dEmi').first.text,
+          emission_type: xml.locate('infCFe/ide/hEmi').first.text,
+          document_qr_code_signature: xml.locate('infCFe/ide/assinaturaQRCODE').first.text,
+          fisco_obs: xml.locate('infCFe/infAdic').first.nodes.map(&:attributes),
+          pdv_number: xml.locate('infCFe/ide/numeroCaixa').first.text,
           company_name: xml.locate('infCFe/emit/xNome').first.text,
-          trading_name: xml.locate('infCFe/emit/xFant').first.text
-          company_address: {},
-          zipcode: xml.locate('infCFe/emit/enderEmit/CEP').first.text
+          company_address: company_address_params,
+          trading_name: xml.locate('infCFe/emit/xFant').first.text,
+          zipcode: xml.locate('infCFe/emit/enderEmit/CEP').first.text,
           cpnj: xml.locate('infCFe/ide/CNPJ').first.text,
           ie: xml.locate('infCFe/emit/enderEmit/IE').first.text,
           im: xml.locate('infCFe/emit/enderEmit/IM').first.text,
           ncfe_number: xml.locate('infCFe/ide/enderEmit/nCFe').first.text,
           products: product_params(xml.locate('infCFe/det')),
-          total: xml.locate('infCFe/total/vProd').first.text
-          cash_back: xml.locate('infCFe/pgto/troco/vProd').first.text
+          total: xml.locate('infCFe/total/vCFe').first.text,
+          total_items: xml.locate('infCFe/det').last.attributes[:nItem],
+          discount: xml.locate('infCFe/total/ICMSTot/vDesc').first.text,
+          cpf: xml.locate('infCFe/dest/CPF').first.text,
+          tributes_value: xml.locate('infCFe/dest/CPF').first.text,
+          payment: {
+            cash_back: xml.locate('infCFe/pgto/troco/vTroco').first.text,
+            payd: xml.locate('infCFe/pgto/troco/vMP').first.text
+          },
+          approximate_value_of_taxex: xml.locate('infCFe/total/vCFeLei12741').first.text
+        }
+      end
+
+      def fisco_obs
+        {
+          text: xml.locate('infCFe/infAdic/obsFisco').first.nodes.first.text,
+          field: xml.locate('infCFe/infAdic/obsFisco').first.attributes
+        }
+      end
+
+      def company_address_params
+        {
+          public_place: xml.locate('infCFe/emit/enderEmit/xLgr').first.value,
+          number: xml.locate('infCFe/emit/enderEmit/nro').first.value,
+          complement: xml.locate('infCFe/emit/enderEmit/xCpl').first.value,
+          city: xml.locate('infCFe/emit/enderEmit/xMun').first.value,
+          neighborhood: xml.locate('infCFe/emit/enderEmit/xBairro').first.value,
+          cep: xml.locate('infCFe/emit/enderEmit/CEP').first.value
         }
       end
 
@@ -46,12 +66,11 @@ module BrInvoicesPdf
           product[:unit_label] = node_product.first.locate('qCom')
           product[:total_value] = node_product.first.locate('vProd')
           product[:unit_value] = node_product.first.locate('vUnCom')
+          product[:discount] = node_product.first.locate('vDesc')
           products << product
         end
         products
       end
-
-
     end
   end
 end
