ef19c2802c5954fa8e38324c87703b52362ef49d
Romario Clacino <romarioclacino@gmail.com>
2017-08-15 15:12:57 -0300

    creation spec to lib/br_invoices_pdf/cfe/parser/document_number.rb

---
+++ spec/lib/br_invoices_pdf/cfe/parser/document_number_spec.rb
@@ -0,0 +1,16 @@
+describe BrInvoicesPdf::Cfe::Parser::DocumentNumber do
+  describe '.execute' do
+    subject { described_class.execute(xml) }
+
+    let(:xml) { Ox.dump(foo: 'bar') }
+
+    before do
+      allow_any_instance_of(BrInvoicesPdf::Cfe::Parser::BaseParser).to receive(:locate_element)
+        .with(xml, 'infCFe/ide/nCFe').and_return(document_number)
+    end
+
+    let(:document_number) { '999' }
+
+    it { expect(subject).to eq(document_number) }
+  end
+end
