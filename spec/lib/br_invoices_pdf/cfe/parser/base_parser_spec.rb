be5ef7780b1b132701b13cd84400e41fcf10a5fa
Romario Clacino <romarioclacino@gmail.com>
2017-08-15 11:13:34 -0300

    refactoring: spec/lib/br_invoices_pdf/cfe/parser/base_parser_spec.rb

--- spec/lib/br_invoices_pdf/cfe/parser/base_parser_spec.rb
+++ spec/lib/br_invoices_pdf/cfe/parser/base_parser_spec.rb
@@ -1,16 +1,12 @@
 describe BrInvoicesPdf::Cfe::Parser::BaseParser do
   let(:xml) { Ox.parse(xml_string) }
   let(:path) { 'middle/bottom' }
-  let(:xml_string) { "<top name='sample'><middle name='second'>#{bottom}</middle></top>" }
-  let(:bottom) { "<bottom name='third'>#{value}</bottom>" }
+  let(:xml_string) { "<top><middle>#{bottom}</middle></top>" }
+  let(:bottom) { "<bottom>#{value}</bottom>" }
   let(:value) { 'some_value' }
 
   describe '.locate_element' do
     subject { described_class.locate_element(xml, 'middle/bottom') }
-
-    let(:xml_string) { "<top><middle>#{bottom}</middle></top>" }
-    let(:bottom) { "<bottom>#{value}</bottom>" }
-    let(:value) { 'some_value' }
 
     context 'when present' do
       it { expect(subject).to eq(value) }
