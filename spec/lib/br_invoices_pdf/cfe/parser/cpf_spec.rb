7008c7d73c1b32129a4a599ab015d6fd0772f816
Romario Clacino <romarioclacino@gmail.com>
2017-08-15 15:12:30 -0300

    refactorig: spec/lib/br_invoices_pdf/cfe/parser/cpf_spec.rb

--- spec/lib/br_invoices_pdf/cfe/parser/cpf_spec.rb
+++ spec/lib/br_invoices_pdf/cfe/parser/cpf_spec.rb
@@ -4,14 +4,10 @@ describe BrInvoicesPdf::Cfe::Parser::Cpf do
 
     let(:xml) { Ox.dump(foo: 'bar') }
 
-    def locate_element_mock(path, value)
+    before do
       base_parser_module = BrInvoicesPdf::Cfe::Parser::BaseParser
       allow_any_instance_of(base_parser_module).to receive(:locate_element)
-        .with(xml, path).and_return(value)
-    end
-
-    before do
-      locate_element_mock('infCFe/dest/CPF', cpf)
+        .with(xml, 'infCFe/dest/CPF').and_return(cpf)
     end
 
     let(:cpf) { '99999999999' }
