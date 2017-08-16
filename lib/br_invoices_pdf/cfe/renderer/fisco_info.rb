a1888645ab2709a219cfb5e1436654d19f8eaa90
Romario Clacino <romarioclacino@gmail.com>
2017-08-16 12:46:54 -0300

    creation spec to lib/br_invoices_pdf/cfe/renderer/fisco_info_obs.rb

--- lib/br_invoices_pdf/cfe/renderer/fisco_info.rb
+++ lib/br_invoices_pdf/cfe/renderer/fisco_info.rb
@@ -1,24 +1,32 @@
 module BrInvoicesPdf
   module Cfe
     module Renderer
-      class FiscoInfo
-        include BaseRenderer
+      module FiscoInfo
+        extend BaseRenderer
 
+        module_function
+
+        # :reek:FeatureEnvy
         def execute(pdf, data)
-          pdf.bounding_box([0, pdf.cursor], width: page_content_width(pdf)) do |_box|
+          pdf.bounding_box([0, pdf.cursor], width: page_content_width(pdf)) do
             pdf.pad(10) do
               pdf.indent(10, 10) do
-                pdf.text("Observações do fisco\n\n", style: :italic)
-
-                data[:fisco_obs].each do |element|
-                  pdf.text(element[:field] + ': ' + element[:text], align: :center)
-                end
+                add_fisco_obs(pdf, data[:fisco_obs])
               end
             end
 
             pdf.stroke_bounds
           end
         end
+
+        def add_fisco_obs(pdf, fisco_obs)
+          pdf.text("Observações do fisco\n\n", style: :italic)
+
+          fisco_obs.each do |element|
+            pdf.text(element[:field] + ': ' + element[:text], align: :center)
+          end
+        end
+        private_class_method :add_fisco_obs
       end
     end
   end
---
+++ spec/lib/br_invoices_pdf/cfe/renderer/fisco_info_spec.rb
@@ -0,0 +1,34 @@
+describe BrInvoicesPdf::Cfe::Renderer::FiscoInfo do
+  describe '.execute' do
+    subject { described_class.execute(pdf, data) }
+    let(:pdf) { double('pdf', cursor: cursor) }
+    let(:cursor) { 10 }
+    let(:data) { { fisco_obs: [fisco_obs] } }
+    let(:fisco_obs) do
+      {
+        field: 'some_field',
+        text: 'Some Text'
+      }
+    end
+    let(:formated_fisco_obs) do
+      "#{fisco_obs[:field]}: #{fisco_obs[:text]}"
+    end
+    let(:base_renderer) { BrInvoicesPdf::Cfe::Renderer::BaseRenderer }
+
+    before do
+      allow_any_instance_of(base_renderer).to receive(:page_content_width).and_return(10)
+    end
+
+    context 'pdf_setup' do
+      it do
+        expect(pdf).to receive(:bounding_box).with([0, cursor], width: 10).and_yield
+        expect(pdf).to receive(:pad).with(10).and_yield
+        expect(pdf).to receive(:indent).with(10, 10).and_yield
+        expect(pdf).to receive(:text).with("Observações do fisco\n\n", style: :italic)
+        expect(pdf).to receive(:text).with(formated_fisco_obs, align: :center)
+        expect(pdf).to receive(:stroke_bounds)
+        subject
+      end
+    end
+  end
+end
