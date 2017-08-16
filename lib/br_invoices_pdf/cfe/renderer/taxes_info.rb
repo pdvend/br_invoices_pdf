03a85947e277068c9d7119eb5d9d1f0e78b3f243
Romario Clacino <romarioclacino@gmail.com>
2017-08-16 17:47:14 -0300

    crerate spec to lib/br_invoices_pdf/cfe/renderer/taxes_info.rb

--- lib/br_invoices_pdf/cfe/renderer/taxes_info.rb
+++ lib/br_invoices_pdf/cfe/renderer/taxes_info.rb
@@ -8,26 +8,31 @@ module BrInvoicesPdf
 
         def execute(pdf, data)
           box(pdf, [0, pdf.cursor], page_content_width(pdf)) do
-            pdf.text("Tributos\n\n", style: :italic)
-
-            tribute_values(pdf, data)
-            date_values(pdf, data)
+            tribute_values(pdf, data[:payment])
+            sat_params = data[:sat_params]
+            sat_number(pdf, sat_params[:sat_number])
+            date_values(pdf, sat_params)
           end
         end
 
         def date_values(pdf, data)
-          time = data[:sat_params][:emission_date] + data[:sat_params][:emission_hour]
+          time = data[:emission_date] + data[:emission_hour]
           pdf.text(DateTime.parse(time).strftime('%d/%m/%Y %H:%M:%S'), align: :center)
         end
         private_class_method :date_values
 
-        def tribute_values(pdf, data)
-          value = format_currency(BigDecimal(data[:payment][:approximate_value_of_taxex]))
+        def tribute_values(pdf, payment)
+          pdf.text("Tributos\n\n", style: :italic)
+          value = format_currency(BigDecimal(payment[:approximate_value_of_taxex]))
           text = "Informação dos tributos totais incidentes (Lei Federal 12.741/2012):\n R$ #{value}\n\n"
           pdf.text(text, align: :center)
-          pdf.text('SAT Número ' + data[:sat_params][:sat_number], align: :center)
         end
         private_class_method :tribute_values
+
+        def sat_number(pdf, sat_number)
+          pdf.text('SAT Número ' + sat_number, align: :center)
+        end
+        private_class_method :sat_number
       end
     end
   end
---
+++ spec/lib/br_invoices_pdf/cfe/renderer/taxes_info_spec.rb
@@ -0,0 +1,40 @@
+describe BrInvoicesPdf::Cfe::Renderer::TaxesInfo do
+  describe '.execute' do
+    subject { described_class.execute(pdf, data) }
+    let(:pdf) { double('pdf', cursor: double('cursor')) }
+    let(:data) { { payment: { approximate_value_of_taxex: 12 }, sat_params: sat_params } }
+    let(:sat_params) do
+      {
+        sat_number: sat_number,
+        emission_date: emission_date,
+        emission_hour: emission_hour
+      }
+    end
+    let(:sat_number) { '123' }
+    let(:emission_date) { '20121212' }
+    let(:emission_hour) { '131313' }
+    let(:base_renderer) { BrInvoicesPdf::Cfe::Renderer::BaseRenderer }
+    let(:width) { 100 }
+    let(:formated_time) { DateTime.parse(emission_date + emission_hour).strftime('%d/%m/%Y %H:%M:%S') }
+    let(:taxes_info) { "Informação dos tributos totais incidentes (Lei Federal 12.741/2012):\n R$ 12,00\n\n" }
+    before do
+      allow_any_instance_of(base_renderer).to receive(:box).and_yield
+      allow_any_instance_of(base_renderer).to receive(:page_content_width).and_return(width)
+      allow(pdf).to receive(:font_size).and_yield
+      allow(pdf).to receive(:move_down)
+      allow(pdf).to receive(:bounding_box)
+    end
+
+    context 'correct infos' do
+      context 'when cpf is present' do
+        it do
+          expect(pdf).to receive(:text).with("Tributos\n\n", style: :italic)
+          expect(pdf).to receive(:text).with(taxes_info, align: :center)
+          expect(pdf).to receive(:text).with("SAT Número #{sat_number}", align: :center)
+          expect(pdf).to receive(:text).with(formated_time, align: :center)
+          subject
+        end
+      end
+    end
+  end
+end
