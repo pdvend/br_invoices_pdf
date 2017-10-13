6a68e44d3f8277e850cf4a4f668cd2b749bbba00
Romario Clacino <romarioclacino@gmail.com>
2017-10-13 14:45:45 -0300

    add lib/br_invoices_pdf/nfce/parser/customer.rb

---
+++ lib/br_invoices_pdf/nfce/parser/customer.rb
@@ -0,0 +1,40 @@
+module BrInvoicesPdf
+  module Nfce
+    module Parser
+      module Customer
+        extend Util::XmlLocate
+
+        module_function
+
+        DEST_ROOT_PATH = "#{Util::XmlLocate::ROOT_PATH}/dest".freeze
+
+        def execute(xml)
+          identification_type = identification_type_by(xml)
+          {
+            identification_type: identification_type,
+            identification: locate_element(xml, "#{DEST_ROOT_PATH}/#{identification_type}"),
+            address: address_params(xml)
+          }
+        end
+
+        def identification_type_by(xml)
+          return 'CNPJ' if locate_element(xml, "#{DEST_ROOT_PATH}/CNPJ")
+          return 'CPF' if locate_element(xml, "#{DEST_ROOT_PATH}/CPF")
+          return 'idEstrangeiro' if locate_element(xml, "#{DEST_ROOT_PATH}/idEstrangeiro")
+        end
+        private_class_method :identification_type_by
+
+        def address_params(xml)
+          {
+            streetname: locate_element(xml, "#{DEST_ROOT_PATH}/enderDest/xLgr"),
+            number: locate_element(xml, "#{DEST_ROOT_PATH}/enderDest/nro"),
+            district: locate_element(xml, "#{DEST_ROOT_PATH}/enderDest/xBairro"),
+            city: locate_element(xml, "#{DEST_ROOT_PATH}/enderDest/xMun"),
+            state: locate_element(xml, "#{DEST_ROOT_PATH}/enderDest/UF")
+          }
+        end
+        private_class_method :address_params
+      end
+    end
+  end
+end
---
+++ spec/lib/br_invoices_pdf/nfce/parser/customer_spec.rb
@@ -0,0 +1,67 @@
+describe BrInvoicesPdf::Nfce::Parser::Customer do
+  describe '.execute' do
+    subject { described_class.execute(xml) }
+
+    let(:xml) { Ox.dump(foo: 'bar') }
+
+    def locate_element_mock(path, value)
+      base_parser_module = BrInvoicesPdf::Util::XmlLocate
+      allow_any_instance_of(base_parser_module).to receive(:locate_element)
+        .with(xml, path).and_return(value)
+    end
+
+    before do
+      locate_element_mock("#{described_class::DEST_ROOT_PATH}/CNPJ", cnpj)
+      locate_element_mock("#{described_class::DEST_ROOT_PATH}/enderDest/xLgr", streetname)
+      locate_element_mock("#{described_class::DEST_ROOT_PATH}/enderDest/nro", number)
+      locate_element_mock("#{described_class::DEST_ROOT_PATH}/enderDest/xBairro", district)
+      locate_element_mock("#{described_class::DEST_ROOT_PATH}/enderDest/xMun", city)
+      locate_element_mock("#{described_class::DEST_ROOT_PATH}/enderDest/UF", state)
+    end
+
+    let(:cnpj) { '99.999.999/9999-99' }
+    let(:zipcode) { '99999-999' }
+    let(:streetname) { 'Public Place' }
+    let(:number) { '99' }
+    let(:district) { 'Some District' }
+    let(:city) { 'Some City' }
+    let(:state) { 'DF' }
+
+    context 'parser correct values' do
+      it { expect(subject[:address][:streetname]).to eq(streetname) }
+      it { expect(subject[:address][:number]).to eq(number) }
+      it { expect(subject[:address][:district]).to eq(district) }
+      it { expect(subject[:address][:city]).to eq(city) }
+      it { expect(subject[:address][:state]).to eq(state) }
+    end
+
+    context 'when CNPJ' do
+      it { expect(subject[:identification_type]).to eq('CNPJ') }
+      it { expect(subject[:identification]).to eq(cnpj) }
+    end
+
+    context 'when CPF' do
+      before do
+        locate_element_mock("#{described_class::DEST_ROOT_PATH}/CNPJ", nil)
+        locate_element_mock("#{described_class::DEST_ROOT_PATH}/CPF", cpf)
+      end
+      let(:cpf) { '11111111111' }
+
+      it { expect(subject[:identification_type]).to eq('CPF') }
+      it { expect(subject[:identification]).to eq(cpf) }
+    end
+
+    context 'when idEstrangeiro' do
+      before do
+        locate_element_mock("#{described_class::DEST_ROOT_PATH}/CNPJ", nil)
+        locate_element_mock("#{described_class::DEST_ROOT_PATH}/CPF", nil)
+        locate_element_mock("#{described_class::DEST_ROOT_PATH}/idEstrangeiro", idEstrangeiro)
+      end
+
+      let(:idEstrangeiro) { '11111111111' }
+
+      it { expect(subject[:identification_type]).to eq('idEstrangeiro') }
+      it { expect(subject[:identification]).to eq(idEstrangeiro) }
+    end
+  end
+end
