fb6f785017dea9bd1081810945ad88209c3a4187
Romario Clacino <romarioclacino@gmail.com>
2017-10-16 11:37:38 -0200

    add spec/lib/br_invoices_pdf/nfce/parser/emission_details_spec.rb

--- lib/br_invoices_pdf/nfce/parser/emission_details.rb
+++ lib/br_invoices_pdf/nfce/parser/emission_details.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module BrInvoicesPdf
   module Nfce
     module Parser
@@ -6,32 +8,31 @@ module BrInvoicesPdf
 
         module_function
 
-        TOTAL_ROOT_PATH = "#{Util::XmlLocate::ROOT_PATH}/ide"
+        EMISSION_ROOT_PATH = "#{Util::XmlLocate::ROOT_PATH}/ide"
 
         EMISSION_TYPES = {
           '1': 'Emissão normal',
           '2': 'Contingência FS-IA',
           '3': 'Contingência SCAN',
           '4': 'Contingência DPEC',
-          '5': 'Contingência FS-DA, com impressão do DANFE em
-          formulário de segurança',
+          '5': 'Contingência FS-DA, com impressão do DANFE em formulário de segurança',
           '6': 'Contingência SVC-AN',
           '7': 'Contingência SVC-RS',
           '9': 'Contingência off-line da NFC-e'
-        }
+        }.freeze
 
         def execute(xml)
           {
-            type: EMISSION_TYPES[locate_element(xml, "#{TOTAL_ROOT_PATH}/tpEmis").to_sym],
-            number: locate_element(xml, "#{TOTAL_ROOT_PATH}/nNF"),
-            serie: locate_element(xml, "#{TOTAL_ROOT_PATH}/serie"),
-            emission_timestamp: Time.new(locate_element(xml, "#{TOTAL_ROOT_PATH}/dhEmi")).utc,
-            receival_timestamp: Time.new(locate_element(xml, "protNFe/infProt/dhRecbto")).utc,
-            #TODO: Identificar como pegar esse valor
+            type: EMISSION_TYPES[locate_element(xml, "#{EMISSION_ROOT_PATH}/tpEmis").to_sym],
+            number: locate_element(xml, "#{EMISSION_ROOT_PATH}/nNF"),
+            serie: locate_element(xml, "#{EMISSION_ROOT_PATH}/serie"),
+            emission_timestamp: Time.new(locate_element(xml, "#{EMISSION_ROOT_PATH}/dhEmi")).utc,
+            receival_timestamp: Time.new(locate_element(xml, 'protNFe/infProt/dhRecbto')).utc,
+            # TODO: Identificar como pegar esse valor
             check_url: nil,
-            access_key: locate_element(xml, "protNFe/infProt/chNFe"),
-            qrcode_url: xml.locate("NFe/infNFeSupl/qrCode").first.nodes.first.value,
-            authorization_protocol: locate_element(xml, "protNFe/infProt/nProt")
+            access_key: locate_element(xml, 'protNFe/infProt/chNFe'),
+            qrcode_url: xml.locate('NFe/infNFeSupl/qrCode').first.nodes.first.value,
+            authorization_protocol: locate_element(xml, 'protNFe/infProt/nProt')
           }
         end
       end
