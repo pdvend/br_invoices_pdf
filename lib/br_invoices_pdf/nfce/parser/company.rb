bf8577c5514e388f34d3021eaa143e3165878fdf
Romario Clacino <romarioclacino@gmail.com>
2017-10-18 09:49:26 -0200

    use MountParams in lib/br_invoices_pdf/nfce/parser/company.rb

--- lib/br_invoices_pdf/nfce/parser/company.rb
+++ lib/br_invoices_pdf/nfce/parser/company.rb
@@ -5,30 +5,27 @@ module BrInvoicesPdf
     module Parser
       module Company
         extend Util::XmlLocate
+        extend Util::MountParams
 
         module_function
 
         EMIT_ROOT_PATH = "#{Util::XmlLocate::ROOT_PATH}/emit"
+        ADDRESS_PARAMS = {
+            streetname: "#{EMIT_ROOT_PATH}/enderEmit/xLgr",
+            number: "#{EMIT_ROOT_PATH}/enderEmit/nro",
+            district: "#{EMIT_ROOT_PATH}/enderEmit/xBairro",
+            city: "#{EMIT_ROOT_PATH}/enderEmit/xMun",
+            state: "#{EMIT_ROOT_PATH}/enderEmit/UF"
+          }.freeze
 
         def execute(xml)
           {
             name: locate_element(xml, "#{EMIT_ROOT_PATH}/xNome"),
             cnpj: locate_element(xml, "#{EMIT_ROOT_PATH}/CNPJ"),
             state_number: locate_element(xml, "#{EMIT_ROOT_PATH}/IE"),
-            address: company_address_params(xml)
+            address: mount(xml, ADDRESS_PARAMS)
           }
         end
-
-        def company_address_params(xml)
-          {
-            streetname: locate_element(xml, "#{EMIT_ROOT_PATH}/enderEmit/xLgr"),
-            number: locate_element(xml, "#{EMIT_ROOT_PATH}/enderEmit/nro"),
-            district: locate_element(xml, "#{EMIT_ROOT_PATH}/enderEmit/xBairro"),
-            city: locate_element(xml, "#{EMIT_ROOT_PATH}/enderEmit/xMun"),
-            state: locate_element(xml, "#{EMIT_ROOT_PATH}/enderEmit/UF")
-          }
-        end
-        private_class_method :company_address_params
       end
     end
   end
