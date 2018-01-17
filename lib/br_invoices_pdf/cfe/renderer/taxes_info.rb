96a2f08d536cbb73cd47b301c11dfd5e9cc54371
Romario Clacino <romarioclacino@gmail.com>
2018-01-17 09:52:00 -0200

    rubocop corections

--- lib/br_invoices_pdf/cfe/renderer/taxes_info.rb
+++ lib/br_invoices_pdf/cfe/renderer/taxes_info.rb
@@ -20,7 +20,7 @@ module BrInvoicesPdf
 
         def date_values(pdf, data)
           time = data[:emission_date] + data[:emission_hour]
-          pdf.text(DateTime.parse(time).strftime('%d/%m/%Y %H:%M:%S'), align: :center)
+          pdf.text(Time.parse(time).strftime('%d/%m/%Y %H:%M:%S'), align: :center)
         end
         private_class_method :date_values
 
